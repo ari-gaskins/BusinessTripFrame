5b50688e6494bc5c1320e14f5ef4c2e74751514f
Ari Gaskins <arigaskins.tech@gmail.com>
2023-07-31 17:16:22 -0400

    setup GUI with labels

--- BusinessTripFrame/src/BusinessReimbursements.java
+++ BusinessTripFrame/src/BusinessReimbursements.java
@@ -20,7 +20,13 @@
 */
 
 
-
+/**
+ * A class intended to establish reimbursement amounts and return those 
+ * values. This class will return values only if certain values' associated
+ * boolean fields are true.
+ * @author Ari Gaskins
+ * @version 1.0
+ */
 public class BusinessReimbursements {
 	// amount paid for meals per day
 	private double mealsPaid = 37.0;
@@ -37,42 +43,148 @@ public class BusinessReimbursements {
 	// rate paid for miles driven in a car rental
 	private double mileageRate = 0.27;
 	
+	// amount reimbursed based on number of miles driven
+	private double milesReimbursed;
+	
 	// number of days the business trip lasts
 	private int numDays;
 	
+	// total reimbursements business person is able to receive
+	private double totalReimbursements;
+	
+	// tracks if user has a car rental for reimbursement
+	private boolean hasRental;
+		
+	// tracks if user gets a taxi
+	private boolean hasTaxi;
+		
+	// tracks if user gets parking fees
+	private boolean hasParking;
+		
+	// tracks if user gets lodging charges
+	private boolean hasLodging;
+	
+	
 	/**
-	 * Constructor for initializing the number of days 
+	 * Constructor for initializing the number of days to 1 when 
+	 * an object is created.
 	 */
 	public BusinessReimbursements() {
 		this.numDays = 1;
+		this.hasRental = false;
+		this.hasTaxi = false;
+		this.hasParking = false;
+		this.hasLodging = false;
 	}
 	
-	public BusinessReimbursements(int numDays) {
+	/**
+	 * Constructor for initializing the number of days to parameter
+	 * and setting the boolean values based on if items exist.
+	 * @paramnumDays int number of days trip lasted
+	 */
+	public BusinessReimbursements(int numDays, boolean hasRental, 
+			boolean hasTaxi, boolean hasParking, boolean hasLodging) {
 		this.numDays = numDays;
+		this.hasRental = hasRental;
+		this.hasTaxi = hasTaxi;
+		this.hasParking = hasParking;
+		this.hasLodging = hasLodging;
 	}
 	
+	/**
+	 * Mutator for milesReimbursed based on number of miles driven entered
+	 * by user.
+	 * @param milesDriven int number of miles driven 
+	 */
+	public void setMilesReimbursed(int milesDriven) {
+		if (hasRental) {
+			milesReimbursed = milesDriven * mileageRate;
+		}
+		else {
+			milesReimbursed = 0;
+		}
+	}
+	
+	/**
+	 * Return milesReimbursed
+	 * @return
+	 */
+	public double getMilesReimbursed() {
+		return milesReimbursed;
+	}
+	
+	/**
+	 * Return mealsPaid
+	 * @return mealsPaid double amount paid for meals per day
+	 */
 	public double getMealsPaid() {
 		return mealsPaid;
 	}
 	
+	/**
+	 * Return parkingPaid
+	 * @return parkingPaid double amount paid for parking per day
+	 */
 	public double getParkingPaid() {
-		return parkingPaid;
+		if (hasParking) {
+			return parkingPaid;
+		}
+		else {
+			return 0;
+		}
 	}
 	
+	/**
+	 * Return taxiPaid
+	 * @return taxiPaid double amount paid for taxi charges
+	 */
 	public double getTaxiPaid() {
-		return taxiPaid;
+		if (hasTaxi) {
+			return taxiPaid;
+		}
+		else {
+			return 0;
+		}
 	}
 	
+	/**
+	 * Return lodgingPaid if user has lodging input 
+	 * @return lodgingPaid double amount paid for lodging charges
+	 */
 	public double getLodgingPaid() {
-		return lodgingPaid;
+		if (hasLodging) {
+			return lodgingPaid;
+		}
+		else {
+			return 0;
+		}
 	}
 	
+	/**
+	 * Return mileageRate 
+	 * @return mileageRate double rate paid for miles driven in a car rental
+	 */
 	public double getMileageRate() {
-		return mileageRate;
+		if (hasRental) {
+			return mileageRate;
+		}
+		else {
+			return 0;
+		}
 	}
 	
+	/**
+	 * Return numDays
+	 * @return numDays int number of days the business trip lasts
+	 */
 	public int getNumDays() {
 		return numDays;
 	}
 	
+	public double getTotalReimbursements() {
+		totalReimbursements = (getMealsPaid() + getParkingPaid() + getTaxiPaid()
+			+ getLodgingPaid()) * numDays;
+		
+		return totalReimbursements;
+	}
 }
--- BusinessTripFrame/src/BusinessTripFrame.java
+++ BusinessTripFrame/src/BusinessTripFrame.java
@@ -19,14 +19,258 @@
 12345678901234567890123456789012345678901234567890123456789012345678901234567890
 */
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-
-public class BusinessTripFrame {
-
+public class BusinessTripFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	// the total amount available for reimbursement
+	private double totalReimbursements;
+		
+	// the total expenses accrued by the user
+	private double totalExpenses;
+		
+	// the total cost that the user must pay
+	private double userCost;
+	
+	// text field for user to enter the cost of airfare
+	private JTextField userAirfare;
+	
+	// text field for user to enter the cost of car rental
+	private JTextField userCarRental;
+	
+	// text field for user to enter # of miles driven for rental
+	private JTextField userMilesDriven;
+	
+	// text field for user to enter the cost of taxi charges
+	private JTextField userTaxiCharges;
+	
+	// text field for user to enter the cost of registration fees
+	private JTextField userRegistrationFees;
+	
+	// text field for user to enter the cost of lodging charges
+	private JTextField userLodgingCharges;
+	
+	// text field for user to enter # of days business trip lasted
+	private JTextField userNumDays;
+	
+	// output of total eligible reimbursements
+	private JTextField userReimbursements;
+	
+	// output of total user input expenses
+	private JTextField userTotalExpenses;
+	
+	// output of user cost after expenses & reimbursements calculated
+	private JTextField userFinalCost;
+	
+	// label for airfare cost
+	private JLabel airfareLabel;
+	
+	// label for car rental cost
+	private JLabel rentalLabel;
+	
+	// label for miles driven cost
+	private JLabel milesLabel;
+	
+	// label for taxi cost
+	private JLabel taxiLabel;
+	
+	// label for registration cost
+	private JLabel registrationLabel;
+	
+	// label for lodging cost
+	private JLabel lodgingLabel;
+	
+	// label for length of trip in days
+	private JLabel numDaysLabel;
+	
+	// label for the final user cost
+	private JLabel finalCostLabel;
+	
+	// label for the total eligible reimbursements
+	private JLabel reimbursementsLabel;
+	
+	// label for the total user expenses
+	private JLabel expensesLabel;
+	
+	// button to submit form data
+	private JButton submitButton = new JButton("Submit");
+	
+	BusinessTripFrame() {
+		// specifies GUI component layout
+		GridBagConstraints layoutConstraints = null;
+		
+		// set frame title
+		setTitle("Business Trip Expense Report");
+		
+		// initialize labels
+		airfareLabel = new JLabel("Cost of Airfare:");
+		rentalLabel = new JLabel("Cost of rental vehicle:");
+		milesLabel = new JLabel("Number of miles driven in rental:");
+		taxiLabel = new JLabel("Cost of taxi charges:");
+		registrationLabel = new JLabel("Cost of registration fees:");
+		lodgingLabel = new JLabel("Cost of lodging:");
+		numDaysLabel = new JLabel("Length of trip (in days):");
+		reimbursementsLabel = new JLabel("Eligble reimbursements:");
+		expensesLabel = new JLabel("Total expenses:");
+		finalCostLabel = new JLabel("Amount you pay:");
+		
+		// initialize text fields
+		userAirfare = new JTextField(20);
+		userAirfare.setEditable(true);
+		
+		userCarRental = new JTextField(20);
+		userCarRental.setEditable(true);
+		
+		userMilesDriven = new JTextField(20);
+		userMilesDriven.setEditable(true);
+		
+		userTaxiCharges = new JTextField(20);
+		userTaxiCharges.setEditable(true);
+		
+		userRegistrationFees = new JTextField(20);
+		userRegistrationFees.setEditable(true);
+		
+		userLodgingCharges = new JTextField(20);
+		userLodgingCharges.setEditable(true);
+		
+		userNumDays = new JTextField(20);
+		userNumDays.setEditable(true);
+		
+		userFinalCost = new JTextField(20);
+		userFinalCost.setEditable(false);
+		userFinalCost.setText(String.valueOf(userCost));
+		
+		userTotalExpenses = new JTextField(20);
+		userTotalExpenses.setEditable(false);
+		userTotalExpenses.setText(String.valueOf(totalExpenses));
+		
+		userReimbursements = new JTextField(20);
+		userReimbursements.setEditable(false);
+		userReimbursements.setText(String.valueOf(totalReimbursements));
+		
+		// Use a GridBagLayout
+		setLayout(new GridBagLayout());
+		
+		// set component's locations: labels
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 0;
+		add(airfareLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 1;
+		add(rentalLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 2;
+		add(milesLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 3;
+		add(taxiLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 4;
+		add(registrationLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 5;
+		add(lodgingLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 6;
+		add(numDaysLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.LAST_LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 7;
+		add(reimbursementsLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.LAST_LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 8;
+		add(expensesLabel, layoutConstraints);
+		
+		layoutConstraints = new GridBagConstraints();
+		layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+		layoutConstraints.insets = new Insets(10, 10, 10, 5);
+		layoutConstraints.anchor = GridBagConstraints.LAST_LINE_START;
+		layoutConstraints.weightx = 0.5;
+		layoutConstraints.weighty = 0.5;
+		layoutConstraints.gridx = 0;
+		layoutConstraints.gridy = 9;
+		add(finalCostLabel, layoutConstraints);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// declares and initializes BusinessTripFrame and its components
+		BusinessTripFrame tripFrame = new BusinessTripFrame();
+		
+		tripFrame.setSize(1650, 1080);
+		tripFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tripFrame.pack();
+		tripFrame.setVisible(true);
 	}
 
 }
